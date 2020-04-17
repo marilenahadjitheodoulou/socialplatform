c2241a1ce729b50a9da7f8fbaf680c7fd7a45384
marilenahadjitheodoulou <it216102@hua.gr>
2020-04-17 17:33:29 +0300

    models

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -1,15 +1,49 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate
+from .models import UserProfile
 
+
+USER_TYPE_CHOICES = (
+    ('CIVILIAN', 'Civilian'),
+    ('NGO', 'N.G.O.')
+)
 
 class UserRegisterForm(UserCreationForm):
-    firstname = forms.CharField()
-    lastname = forms.CharField()
-    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    email = forms.EmailField(required=True)
+    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES)
+    
 
     class Meta:
         model = User
-        fields = ['username', 'firstname', 'lastname', 'email', 'password1', 'password2']
+        fields = ['user_type', 'username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.user_type = self.cleaned_data['user_type']
+
+        if commit:
+            user.save()
+        return user
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('location', 'age')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        user.location = self.cleaned_data['location']
+        user.age = self.cleaned_data['age']
+
+        if commit:
+            user.save()
+        return user
+        
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
-from exercise import templates
+from .forms import UserRegisterForm, UserProfileForm
+from django.contrib.auth import authenticate, login
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+        profile_form = UserProfileForm(request.POST)
+
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            profile.save()
+
             username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
             messages.success(request, f'Your account has been created! You are now able to log in')
+            login(request, user)
+
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'userprofiles/register.html', {'form': form})
+        profile_form = UserProfileForm()
+
+    context = {'form' : form, 'profile_form' : profile_form}
+    return render(request, 'userprofiles/register.html', context)
 
 @login_required
 def profile(request):
